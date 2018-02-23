902fb06eb6c1dd233b2838cafde3e97af61871df
kev-chen <kevchen21@gmail.com>
2018-02-23 07:57:51 -0500

    Initial Server
    Made the server objects and established connections

---
+++ CS451Checkers/Assets/Script/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance { set; get; }
+
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
+
+	void Start () {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	public void ConenctButton()
+    {
+        Debug.Log("Connect");
+    }
+
+    public void HostButton()
+    {
+        try
+        {
+            // Create Server on host
+            Server s = Instantiate(serverPrefab).GetComponent<Server>();
+            s.Init();
+
+            // Create Client on host
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.ConnectToServer("127.0.0.1", 1234);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Debug.Log("Host");
+    }
+
+    public void ConnectToServerButton()
+    {
+        string hostAddress = GameObject.Find("IPInput").GetComponent<InputField>().text;
+        
+        if (string.IsNullOrEmpty(hostAddress))
+            hostAddress = "127.0.0.1";
+
+        try
+        {
+
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.ConnectToServer(hostAddress, 1234);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    public void BackButton()
+    {
+
+    }
+	
+}
---
+++ CS451Checkers/Assets/Script/Server.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    public int port = 1234;
+
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectList;
+
+    private TcpListener server;
+    private bool serverStarted;
+
+    // Called when creating server
+    public void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+
+        try
+        {
+            // Accept any IP Address as long as the port matches
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            serverStarted = true;
+            StartListening();
+            
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+    private void Update()
+    {
+        if (!serverStarted)
+            return;
+
+        foreach (ServerClient c in clients)
+        {
+            // Is the client still connected?
+            if (!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectList.Add(c);
+                continue;
+            }
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                        OnIncomingData(c, data);
+                }
+            }
+        }
+
+        for (int i = disconnectList.Count - 1; i >= 0; i--)
+        {
+            // Tell our player somebody has disconnected
+            clients.Remove(disconnectList[i]);
+            disconnectList.RemoveAt(i);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        ServerClient sc = new ServerClient(listener.EndAcceptTcpClient(ar));
+        clients.Add(sc);
+
+        StartListening();
+
+        Debug.Log("Somebody Connected");
+    }
+
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+
+                return true;
+            }
+            else
+                return false;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    // Server send
+    private void Broadcast(string data, List<ServerClient> cl)
+    {
+        foreach (ServerClient sc in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(sc.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Write error : " + e.Message);
+            }
+        }
+    }
+
+    // Server read
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        Debug.Log(c.clientName + " : " + data);
+    }
+}
+
+
+// Definition of client
+public class ServerClient
+{
+    public string clientName;
+    public TcpClient tcp;
+
+    public ServerClient(TcpClient tcp)
+    {
+        this.tcp = tcp;
+    }
+}
