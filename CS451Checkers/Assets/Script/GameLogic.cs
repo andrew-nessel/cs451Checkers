63ad2b5362cc86802a17aa82dbf5c6d5393a0f90
andrew-nessel <andrew.c.nessel@gmail.com>
2018-03-02 12:53:22 -0500

    Added GameLogic and updated the piece code

---
+++ CS451Checkers/Assets/Script/GameLogic.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    public class GameLogic
+    {
+        protected Piece[][] myBoard;
+
+
+        void initBoard()
+        {
+            myBoard = new Piece[8][];
+
+            for (int x = 0; x < 8; x++)
+            {
+                myBoard[x] = new Piece[8];
+            }
+
+
+            //add black pieces
+            int z = 0;
+            for (int y = 0; y < 3; y++)
+            {
+                int x = 0;
+                while (x < 8)
+                {
+                    if (y % 2 == 0)
+                    {
+                        myBoard[y][x] = new Piece(z, y, x + 1, false);
+                        x += 2;
+                    }
+                    else
+                    {
+                        myBoard[y][x] = new Piece(z, y, x, false);
+                        x += 2;
+                    }
+                    z++;
+                }
+            }
+
+            //add white peices
+            for (int y = 5; y < 8; y++)
+            {
+                int x = 0;
+                while (x < 8)
+                {
+                    if (y % 2 == 0)
+                    {
+                        myBoard[y][x] = new Piece(z, y, x + 1, true);
+                        x += 2;
+                    }
+                    else
+                    {
+                        myBoard[y][x] = new Piece(z, y, x, true);
+                        x += 2;
+                    }
+                    z++;
+                }
+            }
+        }
+
+        bool validMove(Piece piece, int x, int y)
+        {
+            int oldx = piece.getX();
+            int oldy = piece.getY();
+
+            //check if its a king, otherwise
+            //check its moving foward
+
+            if (!piece.isKing())
+            {
+                if (piece.isWhite() && y > oldy)
+                {
+                    return false;
+                }
+                else if ((!piece.isWhite()) && y < oldy)
+                {
+                    return false;
+                }
+            }
+
+            int xdiff = oldx - x;
+            int ydiff = oldy - y;
+            //check if the movement is diagonal
+            if (xdiff != ydiff)
+            {
+                return false;
+            }
+
+            //check if the movement is more than one space that there is a piece to capture in between NOT DONE
+
+            return false;
+        }
+
+        bool makeMove(Piece piece, int x, int y)
+        {
+            //if(!validMove(piece, x, y)){
+            //return false;
+            //}
+
+            //if there are peices in between new and old capture peices NOT DONE
+
+            //if need be king the piece
+            if (y == 7)
+            {
+                piece.kingMe();
+            }
+
+
+            //update x and y
+            piece.updateX(x);
+            piece.updateY(y);
+
+            return false;
+        }
+
+        public Piece[][] getBoard()
+        {
+            return myBoard;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
--- CS451Checkers/Assets/Script/piece.cs
+++ CS451Checkers/Assets/Script/piece.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class piece : MonoBehaviour {
+public class Piece : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    protected int ID;
+    protected bool white;
+    protected bool king;
+    protected int x;
+    protected int y;
+
+    public Piece(int ID, int y, int x, bool isWhite)
+    {
+        this.ID = ID;
+        this.white = isWhite;
+        this.king = false;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void updateX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void updateY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public bool isWhite()
+    {
+        return white;
+    }
+
+    public bool isKing()
+    {
+        return king;
+    }
+
+    public void kingMe()
+    {
+        king = true;
+    }
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
